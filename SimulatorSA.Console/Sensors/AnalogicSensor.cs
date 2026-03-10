8011bfc1fbefa58af7189bc292efede725cffe11
CAIO HOLANDA <caiojholanda@gmail.com>
2026-03-10 22:17:32 +0000

    WIP:First Classes

--- SimulatorSA.Console/Sensors/AnalogicSensor.cs
+++ SimulatorSA.Console/Sensors/AnalogicSensor.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using SimulatorSA.Console.Enumerators;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SimulatorSA.Console.Sensors
 {
-    internal class AnalogicSensor
+    public class AnalogicSensor
     {
+        public string Name { get; set; }
+        public double MinValue { get; set; }
+        public double MaxValue { get; set; }
+        public SinalType SinalType { get; set; }
+
+
     }
 }
