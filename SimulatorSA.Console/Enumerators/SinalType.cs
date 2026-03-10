1517561f4922a5d58b1767bc3d195f0d626b4ed2
CAIO HOLANDA <caiojholanda@gmail.com>
2026-03-10 22:06:23 +0000

    Initial Commit
    Initial File and Folder Structure

---
+++ SimulatorSA.Console/Enumerators/SinalType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimulatorSA.Console.Enumerators
+{
+    public enum SinalType
+    {
+        Current4to20mA,
+        Voltage0to10V
+    }
+}
