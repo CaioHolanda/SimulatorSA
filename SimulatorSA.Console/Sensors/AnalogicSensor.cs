96076fdcbd8d50288c6709ca5cd05f8ec46e1432
Caio Holanda <caiojholanda@gmail.com>
2026-03-11 16:50:59 +0000

    WIP: Sensor Class
    Building a model of the Sensors

--- SimulatorSA.Console/Sensors/AnalogicSensor.cs
+++ SimulatorSA.Console/Sensors/AnalogicSensor.cs
@@ -12,6 +12,29 @@ namespace SimulatorSA.Console.Sensors
         public double MaxValue { get; set; }
         public SinalType SinalType { get; set; }
 
+        public AnalogicSensor(string name, double minValue, double maxValue, SinalType sinalType)
+        {
+            Name = name;
+            MinValue = minValue;
+            MaxValue = maxValue;
+            SinalType = sinalType;
+        }
 
+        public double LimitValue(double value)
+        {
+            if (value < MinValue) return MinValue;
+            if (value > MaxValue) return MaxValue;
+            return value;
+        }
+
+        public double ConverterToSignal(double physicalValue)
+        {
+            physicalValue = LimitValue(physicalValue);
+            double percentual = (physicalValue - MinValue) / (MaxValue - MinValue);
+            return SinalType switch
+            {
+
+            }
+        }
     }
 }
